15ca5a65fb0225c4d606bee4f603d992330dafba
Nicolò Malucelli <nicolo.malucelli@studio.unibo.it>
2024-01-08 15:13:40 +0100

    update index.html

--- client/src/main.ts
+++ client/src/main.ts
@@ -2,6 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap"
+import 'bootstrap-icons/font/bootstrap-icons.css'
+
 
 const app = createApp(App);
 
